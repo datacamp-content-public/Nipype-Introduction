fa8987d4f0b961afb89573eb9aaf4c4c6e502904
James Kent <james-kent@uiowa.edu>
2018-03-10 11:22:51 -0600

    remove sudo

--- requirements.sh
+++ requirements.sh
@@ -7,7 +7,7 @@ apt-get install -y apt-transport-https \
      gnupg2 \
      software-properties-common
 
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
