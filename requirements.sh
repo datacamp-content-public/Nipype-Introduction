cde2618b25096e21f6e076d4d4cb5110107aea87
James Kent <james-kent@uiowa.edu>
2018-03-10 10:55:13 -0600

    add docker as requirement

--- requirements.sh
+++ requirements.sh
@@ -2,9 +2,7 @@
 set -e
 
 ##### Install specific package versions with pip #####
-
+pip3 install docker
 # pip3 install pandas==0.22.0
 # pip3 install matplotlib==2.1.2
 # pip3 install scikit-learn==0.19.1
-
-
