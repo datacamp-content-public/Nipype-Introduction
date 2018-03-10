6a07fef3b93af271067e28293a85679c17b777e2
James Kent <james-kent@uiowa.edu>
2018-03-10 11:06:06 -0600

    add apt-get command to requirements

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,10 @@
 # If bash command fails, build should error out
 set -e
-
+apt-get install apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
 ##### Install specific package versions with pip #####
 pip3 install docker
 # pip3 install pandas==0.22.0
