3688955228ebfeddfe5c9183969d2b4aa715cbfb
James Kent <james-kent@uiowa.edu>
2018-03-10 11:46:38 -0600

    get neurodebian working on the container

--- requirements.sh
+++ requirements.sh
@@ -1,27 +1,42 @@
 # If bash command fails, build should error out
 set -e
 
+# https://bugs.debian.org/830696 (apt uses gpgv by default in newer releases, rather than gpg)
+set -x \
+	&& apt-get update \
+	&& { \
+		which gpg \
+		|| apt-get install -y --no-install-recommends gnupg \
+	; } \
+# Ubuntu includes "gnupg" (not "gnupg2", but still 2.x), but not dirmngr, and gnupg 2.x requires dirmngr
+# so, if we're not running gnupg 1.x, explicitly install dirmngr too
+	&& { \
+		gpg --version | grep -q '^gpg (GnuPG) 1\.' \
+		|| apt-get install -y --no-install-recommends dirmngr \
+	; } \
+	&& rm -rf /var/lib/apt/lists/*
+
+# apt-key is a bit finicky during "docker build" with gnupg 2.x, so install the repo key the same way debian-archive-keyring does (/etc/apt/trusted.gpg.d)
+# this makes "apt-key list" output prettier too!
+set -x \
+	&& export GNUPGHOME="$(mktemp -d)" \
+	&& gpg --keyserver ha.pool.sks-keyservers.net --recv-keys DD95CC430502E37EF840ACEEA5D32F012649A5A9 \
+	&& gpg --export DD95CC430502E37EF840ACEEA5D32F012649A5A9 > /etc/apt/trusted.gpg.d/neurodebian.gpg \
+	&& rm -rf "$GNUPGHOME" \
+	&& apt-key list | grep neurodebian
+
+{ \
+	echo 'deb http://neuro.debian.net/debian trusty main'; \
+	echo 'deb http://neuro.debian.net/debian data main'; \
+	echo '#deb-src http://neuro.debian.net/debian-devel trusty main'; \
+} > /etc/apt/sources.list.d/neurodebian.sources.list
+
+sed -i -e 's,main *$,main contrib non-free,g' /etc/apt/sources.list.d/neurodebian.sources.list; grep -q 'deb .* multiverse$' /etc/apt/sources.list || sed -i -e 's,universe *$,universe multiverse,g' /etc/apt/sources.list
+
 apt-get update
-
-apt-get install -y apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
-
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-   $(lsb_release -cs) \
-   stable"
-
-apt-get update
-
-apt-get install -y docker-ce
-
+apt-get install -y fsl
 ##### Install specific package versions with pip #####
-pip3 install docker
+pip3 install nipype
 # pip3 install pandas==0.22.0
 # pip3 install matplotlib==2.1.2
 # pip3 install scikit-learn==0.19.1
